135db45a7ea60eabc628e13456a21b63b8081e81
Indrek Juhkam <indrek@urgas.eu>
2010-10-29 21:05:11 +0300

    listen tab changes

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,33 +1,59 @@
+var uturgasUrlBarListener = {
+  QueryInterface: function(aIID) {
+    if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+        aIID.equals(Components.interfaces.nsISupports))
+      return this;
+    throw Components.results.NS_NOINTERFACE;
+  },
+
+  onLocationChange: function(aProgress, aRequest, aURI) {
+    uturgas.onPageChange(aURI);
+  },
+
+  onStateChange: function(a, b, c, d) {},
+  onProgressChange: function(a, b, c, d, e, f) {},
+  onStatusChange: function(a, b, c, d) {},
+  onSecurityChange: function(a, b, c) {}
+};
+
 var uturgas = {
   theDot: null,
+  currentPage: null,
 
   onFirefoxLoad: function(event) {
     this.initialized = true;
     this.theDot = document.getElementById("uturgas-statusbar-dot");
 
     this.strings = document.getElementById("uturgas-strings");
-    // usage: this.strings.getString("helloMessageTitle")*/
+    // usage: this.strings.getString("helloMessageTitle")
 
-    var appcontent = document.getElementById("appcontent"); // browser  
-    if (appcontent)
-      appcontent.addEventListener("DOMContentLoaded", function(e) { uturgas.onPageLoad(e); }, true);
+    gBrowser.addProgressListener(
+      uturgasUrlBarListener, 
+      Components.interfaces.nsIWebProgress.NOTIFY_LOCATION
+    );
   },
 
-  onPageLoad: function(event) {
-    var doc = event.originalTarget; // doc is document that triggered "onload" event
-
-    if (doc.location.href.search("google") > -1) {
-      uturgas.theDot.style.color = "green";
+  onPageChange: function(aURI) {
+    var href = aURI.spec;
+    if (href.match(/moodle\.ut\.ee.*review\.php\?q=(\d+)&attempt=(\d+)/)) {
+      this.currentPage = "moodle";
+    } else if (href.match(/webct\.e\-uni\.ee.*attempt=(\d+)/)) {
+      this.currentPage = "webct";
+    } else if (href.match(/webct\.e\-uni\.ee/)) {
+      // TODO: check iframe
+      this.currentPage = null;
+    } else if (href.match(/google/)) {
+      this.currentPage = "google"; // this is temporary just for testing
+    } else {
+      this.currentPage = null;
     }
 
-    // add event listener for page unload
-    event.originalTarget.defaultView.addEventListener("unload", function() { 
-      uturgas.onPageUnload(); 
-    }, true);
-  },
-
-  onPageUnload: function(event) {
-    uturgas.theDot.style.color = "black";
+    if (this.currentPage) {
+      this.theDot.style.color = "yellow";
+    } else {
+      this.theDot.style.color = "black";
+    }
   }
 };
 
