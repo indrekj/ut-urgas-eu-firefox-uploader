671770768a4128fc6645c2354c086c7e4d2b299d
Indrek Juhkam <indrek@urgas.eu>
2010-10-30 18:39:20 +0300

    added basic uploading functionality

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -17,6 +17,65 @@ var uturgasUrlBarListener = {
   onSecurityChange: function(a, b, c) {}
 };
 
+var uturgasUploader = {
+  uploadURL: "http://localhost:3000/assessments",
+
+  upload: function(content, attemptId) {
+    // generate boundary string
+    var boundaryString = "ut-urgas-eu-boundary-" + Math.random();
+    var boundary = "--" + boundaryString;
+    var filename = "TODO.html";
+
+    // create a string input stream with the form preamble
+    var formData = boundary + '\r\n' +
+      'Content-Disposition: form-data; name="file"; filename="' + filename + '"' + '\r\n' +
+      'Content-Type: text/html' + '\r\n\r\n';
+    var prefixStringInputStream = this.stringToStream(formData);
+
+    // convert content to input stream
+    var contentStringInputStream = this.stringToStream(content);
+
+    // write out the rest of the form to another string input stream
+    var suffixStringInputStream = this.stringToStream("\r\n" + boundary + "\r\n");
+
+    // multiplex the streams together
+    var uploadStream = Components.
+      classes["@mozilla.org/io/multiplex-input-stream;1"].
+      createInstance(Components.interfaces.nsIMultiplexInputStream);
+    uploadStream.appendStream(prefixStringInputStream);
+    uploadStream.appendStream(contentStringInputStream);
+    uploadStream.appendStream(suffixStringInputStream);
+
+    // post it
+    var req = new XMLHttpRequest();
+    req.open("POST", this.uploadURL);
+    req.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+    req.setRequestHeader("Content-Length", uploadStream.available());
+    req.send(uploadStream);
+  },
+
+  // from: http://www.chrisfinke.com/2010/01/30/
+  stringToStream: function(str) {
+    function encodeToUtf8(oStr) {
+      var utfStr = oStr;
+      var uConv = Components.
+        classes["@mozilla.org/intl/scriptableunicodeconverter"].
+        createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+      uConv.charset = "UTF-8";
+      utfStr = uConv.ConvertFromUnicode(oStr);
+
+      return utfStr;
+    }
+    
+    str = encodeToUtf8(str);
+    var stream = Components.
+      classes["@mozilla.org/io/string-input-stream;1"].
+      createInstance(Components.interfaces.nsIStringInputStream);
+    stream.setData(str, str.length);
+    return stream;
+  }
+}
+
 var uturgas = {
   theDot: null,
   currentPage: null,
