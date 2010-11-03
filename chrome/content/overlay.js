c37ec7aa0af1e8930a76be3608cc015bcd8a7bd2
Indrek Juhkam <indrek@urgas.eu>
2010-11-04 01:14:28 +0200

    added confirmation dialog. refactored http post request

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -22,74 +22,32 @@ var uturgasUploader = {
   existsURL: "http://localhost:3000/assessments/exists",
 
   upload: function(file, attemptId) {
-    // generate boundary string
-    var boundaryString = "ut-urgas-eu-boundary-" + Math.random();
-    var boundary = "--" + boundaryString;
-    var filename = "TODO.html";
-
-    // create a string input stream with the form preamble
-    var formData = boundary + '\r\n' +
-      'Content-Disposition: form-data; name="file"; filename="' + filename + '"' + '\r\n' +
-      'Content-Type: text/html' + '\r\n\r\n';
-    var prefixStringInputStream = this.stringToStream(formData);
-
-    // convert content to input stream
-    var contentStringInputStream = this.fileToStream(file);
-
-    // write out the rest of the form to another string input stream
-    var suffixStringInputStream = this.stringToStream("\r\n" + boundary + "\r\n");
-
-    // multiplex the streams together
-    var uploadStream = Cc["@mozilla.org/io/multiplex-input-stream;1"].
-      createInstance(Ci.nsIMultiplexInputStream);
-    uploadStream.appendStream(prefixStringInputStream);
-    uploadStream.appendStream(contentStringInputStream);
-    uploadStream.appendStream(suffixStringInputStream);
+    var postRequest = createPostRequest({
+      "assessment[attempt_id]": attemptId,
+      "file": {
+        "file": file
+      }
+    });
 
     // post it
     var req = new XMLHttpRequest();
     req.open("POST", this.uploadURL);
-    req.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundaryString);
-    req.setRequestHeader("Content-Length", uploadStream.available());
-    req.send(uploadStream);
-  },
-  
-  // from: http://www.chrisfinke.com/2010/01/30/
-  fileToStream: function(file) {
-    if (typeof file == "string") {
-      var fpLocal = Cc['@mozilla.org/file/local;1'].
-        createInstance(Ci.nsILocalFile);
-      fpLocal.initWithFile(file);
-    } else {
-      var fpLocal = file;
-    }
-
-    var finStream = Cc["@mozilla.org/network/file-input-stream;1"].
-      createInstance(Ci.nsIFileInputStream);
-    finStream.init(fpLocal, 1, 0, false);
-    var bufStream = Cc["@mozilla.org/network/buffered-input-stream;1"].
-      createInstance(Ci.nsIBufferedInputStream);
-    bufStream.init(finStream, 9000000);
-    return bufStream;
-  },
-
-  // from: http://www.chrisfinke.com/2010/01/30/
-  stringToStream: function(str) {
-    function encodeToUtf8(oStr) {
-      var utfStr = oStr;
-      var uConv = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
-        createInstance(Ci.nsIScriptableUnicodeConverter);
-      uConv.charset = "UTF-8";
-      utfStr = uConv.ConvertFromUnicode(oStr);
-
-      return utfStr;
-    }
-    
-    str = encodeToUtf8(str);
-    var stream = Cc["@mozilla.org/io/string-input-stream;1"].
-      createInstance(Ci.nsIStringInputStream);
-    stream.setData(str, str.length);
-    return stream;
+    req.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + postRequest.boundary);
+    req.setRequestHeader("Content-Length", postRequest.requestBody.available());
+    req.onreadystatechange = function() {
+      if (req.readyState == 4 && req.status == 200) {
+        var res = eval('(' + req.responseText + ')');
+        var features = "chrome,titlebar,toolbar,centerscreen,modal";
+        window.openDialog(
+          "chrome://uturgas/content/confirmation.xul", 
+          "Töö atribuudid", 
+          features,
+          res.assessment.title,
+          res.assessment.category
+        );      
+      }
+    };
+    req.send(postRequest.requestBody);
   }
 }
 
@@ -120,8 +78,8 @@ var uturgas = {
     this.attemptId = null;
     this.theDot.src = "chrome://uturgas/skin/black.png";
     this.menuAdd.disabled = true;
-
     this.currentUri = aURI;
+
     var href = aURI.spec;
 
     var res = href.match(/moodle\.ut\.ee.*review\.php\?attempt=(\d+)&showall=true/);
@@ -194,17 +152,28 @@ var uturgas = {
   },
 
   startUpload: function() {
-    var file = Cc["@mozilla.org/file/local;1"].
-      createInstance(Ci.nsILocalFile);  
-    file.initWithPath("/tmp/ut-urgas-eu-test.html");  
+    var file = Cc["@mozilla.org/file/directory_service;1"].  
+      getService(Ci.nsIProperties).  
+      get("TmpD", Ci.nsIFile);  
+
+    file.append("ut-urgas-eu-" + this.currentPage + "-" + this.attemptId + ".html");
+    file.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);  
+    
     var wbp = Cc['@mozilla.org/embedding/browser/nsWebBrowserPersist;1'].
       createInstance(Ci.nsIWebBrowserPersist);
-
-    // don't save gzipped
-    wbp.persistFlags &= ~Ci.nsIWebBrowserPersist.PERSIST_FLAGS_NO_CONVERSION;  
+    wbp.persistFlags &= ~Ci.nsIWebBrowserPersist.PERSIST_FLAGS_NO_CONVERSION;
+    wbp.progressListener = {
+      onProgressChange: function(aWebProgress, aRequest, aCurSelfProgress, 
+          aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress, aDownload) {
+      },
+      onStateChange: function(aWebProgress, aRequest, aStateFlags, aStatus, aDownload) {
+        alert(aStatus);
+        if (aStatus & Components.interfaces.nsIWebProgressListener.STATE_STOP)
+          alert("stop");
+          //uturgasUploader.upload(file, this.attemptId);
+      }
+    };
     wbp.saveURI(this.currentUri, null, null, null, null, file); 
-
-    uturgasUploader.upload(file, this.attemptId);
   },
 
   openHelpDialog: function() {
@@ -212,6 +181,117 @@ var uturgas = {
     window.openDialog("chrome://uturgas/content/help.xul", "Kasutusjuhend", features);
   }
 };
+
+// http://www.chrisfinke.com/2010/01/30/
+function createPostRequest(args) {
+  /**
+   * Generates a POST request body for uploading.
+   *
+   * args is an associative array of the form fields.
+   *
+   * Example:
+   * var args = { "field1": "abc", "field2" : "def", "fileField" :
+   *              { "file": theFile, "headers" : [ "X-Fake-Header: foo" ] } };
+   *
+   * theFile is an nsILocalFile; the headers param for the file field is optional.
+   *
+   * This function returns an array like this:
+   * { "requestBody" : uploadStream, "boundary" : BOUNDARY }
+   *
+   * To upload:
+   *
+   * var postRequest = createPostRequest(args);
+   * var req = new XMLHttpRequest();
+   * req.open("POST", ...);
+   * req.setRequestHeader("Content-Type","multipart/form-data; boundary="+postRequest.boundary);
+   * req.setRequestHeader("Content-Length", (postRequest.requestBody.available()));
+   * req.send(postRequest.requestBody);
+   */
+
+  function fileToStream(file) {
+    var fpLocal = Cc['@mozilla.org/file/local;1'].
+      createInstance(Ci.nsILocalFile);
+    fpLocal.initWithFile(file);
+    var finStream = Cc["@mozilla.org/network/file-input-stream;1"].
+      createInstance(Ci.nsIFileInputStream);
+    finStream.init(fpLocal, 1, 0, false);
+    var bufStream = Cc["@mozilla.org/network/buffered-input-stream;1"].
+      createInstance(Ci.nsIBufferedInputStream);
+    bufStream.init(finStream, 9000000);
+    return bufStream;
+  }
+
+  function stringToStream(str) {
+    function encodeToUtf8(oStr) {
+      var utfStr = oStr;
+      var uConv = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
+        createInstance(Ci.nsIScriptableUnicodeConverter);
+      uConv.charset = "UTF-8";
+      utfStr = uConv.ConvertFromUnicode(oStr);
+
+      return utfStr;
+    }
+    
+    str = encodeToUtf8(str);
+    var stream = Cc["@mozilla.org/io/string-input-stream;1"].
+      createInstance(Ci.nsIStringInputStream);
+    stream.setData(str, str.length);
+    return stream;
+  }
+
+  var mimeSvc = Cc["@mozilla.org/mime;1"].
+    getService(Components.interfaces.nsIMIMEService);
+  const BOUNDARY = "----------------" + Math.random(); 
+
+  var streams = [];
+
+  for (var i in args) {
+    var buffer = "--" + BOUNDARY + "\r\n";
+    buffer += "Content-Disposition: form-data; name=\"" + i + "\"";
+    streams.push(stringToStream(buffer));
+
+    if (args[i] == null) {
+      buffer = "\r\n\r\n\r\n";
+      streams.push(stringToStream(buffer));
+    } else if (typeof args[i] == "object") {
+      buffer = "; filename=\"" + args[i].file.leafName + "\"";
+
+      if ("headers" in args[i]) {
+        if (args[i].headers.length > 0) {
+          for (var q = 0; q < args[i].headers.length; q++){
+            buffer += "\r\n" + args[i].headers[q];
+          }
+        }
+      }
+
+      var theMimeType = mimeSvc.getTypeFromFile(args[i].file);
+
+      buffer += "\r\nContent-Type: " + theMimeType;
+      buffer += "\r\n\r\n";
+
+      streams.push(stringToStream(buffer));
+
+      streams.push(fileToStream(args[i].file));
+    } else {
+      buffer = "\r\n\r\n";
+      buffer += args[i];
+      buffer += "\r\n";
+      streams.push(stringToStream(buffer));
+    }
+  }
+
+  var buffer = "--" + BOUNDARY + "--\r\n";
+  streams.push(stringToStream(buffer));
+
+  var uploadStream = Cc["@mozilla.org/io/multiplex-input-stream;1"].
+    createInstance(Ci.nsIMultiplexInputStream);
+
+  for (var i = 0; i < streams.length; i++) {
+    uploadStream.appendStream(streams[i]);
+  }
+
+  return {"requestBody" : uploadStream, "boundary": BOUNDARY};
+}
 
 window.addEventListener("load", function() {
   uturgas.onFirefoxLoad()
